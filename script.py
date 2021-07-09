ed3eb94ea628b5946f847ecd35f53741bcd35e44
BuildTools <randysim9@gmail.com>
2021-07-09 19:40:20 -0400

    found panel urls

--- script.py
+++ script.py
@@ -1,13 +1,32 @@
-import requests;
+import requests
+import re
+
+debugUrl = "https://m.comic.naver.com/webtoon/detail?titleId=756137&no=17&week=thu&listSortOrder=DESC&listPage=1"
+prod = False
 
 def main():
-    comicUrl = input("Naver Webtoon Url: ")
+    comicUrl = debugUrl
+
+    if prod:
+        comicUrl = input("Naver Webtoon Url: ")
+
     print("Fetching from: " + comicUrl)
 
-    res = requests.get(comicUrl);
-    data = res.json()
+    data = requests.get(comicUrl).text
+    cutOne = data.find("<ul>")
+    cutTwo = data.find("</ul>")
 
-    print(data)
+    listComp = data[cutOne + 4:cutTwo]
+    dataIndexes = [m.start() for m in re.finditer("data-src", listComp)]
+    urls = []
+
+    for index in dataIndexes:
+        firstQuote = listComp.find('"', index)
+        secondQuote = listComp.find('"', firstQuote+1)
+
+        urls.append(listComp[firstQuote:secondQuote])
+
+    print(urls)
 
 
 main()
