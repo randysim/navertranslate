0f65244628bd2ff5c7c997aedd99fabdee604c36
BuildTools <randysim9@gmail.com>
2021-07-09 22:40:01 -0400

    skidding done

--- script.py
+++ script.py
@@ -1,31 +1,55 @@
 import requests
 import re
-import cv2
 import os
-import urllib.request
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 
 debugUrl = "https://m.comic.naver.com/webtoon/detail?titleId=756137&no=17&week=thu&listSortOrder=DESC&listPage=1"
 prod = False
 scriptPath = "C:/Users/phone/PycharmProjects/navertranslate"
 
+tesseractPath = "C:\\Users\\phone\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = tesseractPath
+
+options = "-l {} --psm {}".format("kor", 3)
+
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename), config=options)  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
 def downloadImages(urls, output):
     counter = 1
+    images = []
 
     for url in urls:
         extension = url.split(".")[-1]
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
-        }
-        response = requests.get(url, headers=headers)
-        with open(scriptPath + "/paneloutput/" + output + "/" + str(counter) + "." + extension, 'wb') as handle:
-            handle.write(response.content)
+        path = scriptPath + "/paneloutput/" + output + "/" + str(counter) + "." + extension
+        isFile = os.path.isfile(path)
+
+        if isFile == False:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
+            }
+            response = requests.get(url, headers=headers)
+            with open(path, 'wb') as handle:
+                handle.write(response.content)
 
         print("DOWNLOADING: " + str(counter) + "/" + str(len(urls)))
         counter += 1
+        images.append(path)
     print("Finished Downloading Panels: " + output)
+    return images
 
 def main():
     print("PATH: " + scriptPath)
+    print("TESSERACT: " + tesseractPath)
 
     comicUrl = debugUrl
 
@@ -50,7 +74,16 @@ def main():
 
     titleId = comicUrl[comicUrl.find("titleId=") + 8 : comicUrl.find("&")]
     print("Title ID: " + titleId)
-    downloadImages(urls, titleId)
+    images = downloadImages(urls, titleId)
+    imageText = ocr_core(images[0])
+    print(images[0])
+    print(imageText)
+
+    """
+    for image in images:
+        imageText = ocr_core(image)
+        print(imageText)
+    """
 
 main()
 
