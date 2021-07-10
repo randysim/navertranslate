ddda79ec05959573168c98d7316ab576f490ce29
BuildTools <randysim9@gmail.com>
2021-07-09 20:43:58 -0400

    downloaded panels

--- script.py
+++ script.py
@@ -1,10 +1,32 @@
 import requests
 import re
+import cv2
+import os
+import urllib.request
 
 debugUrl = "https://m.comic.naver.com/webtoon/detail?titleId=756137&no=17&week=thu&listSortOrder=DESC&listPage=1"
 prod = False
+scriptPath = "C:/Users/phone/PycharmProjects/navertranslate"
+
+def downloadImages(urls, output):
+    counter = 1
+
+    for url in urls:
+        extension = url.split(".")[-1]
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
+        }
+        response = requests.get(url, headers=headers)
+        with open(scriptPath + "/paneloutput/" + output + "/" + str(counter) + "." + extension, 'wb') as handle:
+            handle.write(response.content)
+
+        print("DOWNLOADING: " + str(counter) + "/" + str(len(urls)))
+        counter += 1
+    print("Finished Downloading Panels: " + output)
 
 def main():
+    print("PATH: " + scriptPath)
+
     comicUrl = debugUrl
 
     if prod:
@@ -24,9 +46,11 @@ def main():
         firstQuote = listComp.find('"', index)
         secondQuote = listComp.find('"', firstQuote+1)
 
-        urls.append(listComp[firstQuote:secondQuote])
+        urls.append(listComp[firstQuote + 1:secondQuote])
 
-    print(urls)
-
+    titleId = comicUrl[comicUrl.find("titleId=") + 8 : comicUrl.find("&")]
+    print("Title ID: " + titleId)
+    downloadImages(urls, titleId)
 
 main()
+
